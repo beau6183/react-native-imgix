9053e5c0371100c039e692190dd99428c5f7f58a
Beau scott <bscott@lifetimefitness.com>
2019-05-11 13:41:47 -0600

    udpated peer deps to react16, rn0.50, and included prop-types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import processImage from './support.js'
 import {Image} from 'react-native'
 
